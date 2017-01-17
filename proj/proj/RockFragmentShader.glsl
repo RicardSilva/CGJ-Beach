b74b882b7efeaf454c56b4279d932561946f097c
Luis Pereira <luisfppereira@tecnico.ulisboa.pt>
2017-01-17 05:54:26 +0000

    Changed Rock Shader

--- proj/proj/RockFragmentShader.glsl
+++ proj/proj/RockFragmentShader.glsl
@@ -10,11 +10,6 @@ uniform vec3 Color;
 uniform sampler2D Texture;
 uniform sampler2D permTexture;
 
-//varying vec3 v_texCoord3D;
-//varying vec4 v_color;
-
-#define ONE 0.00390625
-#define ONEHALF 0.001953125
 
 uniform mat4 ModelMatrix;
 uniform SharedMatrices
@@ -28,66 +23,108 @@ uniform SharedMatrices
 	float ambientCoefficient; 
 };
 
-float fade(float t) {
-	return t*t*t*(t*(t*6.0-15.0)+10.0);
-}
+vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
+vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
 
-float noise(vec2 P)
-{
-  vec2 Pi = ONE*floor(P)+ONEHALF; // Integer part, scaled and offset for texture lookup
-  vec2 Pf = fract(P);             // Fractional part for interpolation
+float cnoise(vec3 P){
+  vec3 Pi0 = floor(P); // Integer part for indexing
+  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+  Pi0 = mod(Pi0, 289.0);
+  Pi1 = mod(Pi1, 289.0);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
 
-  // Noise contribution from lower left corner
-  vec2 grad00 = texture(permTexture, Pi).rg * 4.0 - 1.0;
-  float n00 = dot(grad00, Pf);
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
 
-  // Noise contribution from lower right corner
-  vec2 grad10 = texture(permTexture, Pi + vec2(ONE, 0.0)).rg * 4.0 - 1.0;
-  float n10 = dot(grad10, Pf - vec2(1.0, 0.0));
+  vec4 gx0 = ixy0 / 7.0;
+  vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
 
-  // Noise contribution from upper left corner
-  vec2 grad01 = texture(permTexture, Pi + vec2(0.0, ONE)).rg * 4.0 - 1.0;
-  float n01 = dot(grad01, Pf - vec2(0.0, 1.0));
+  vec4 gx1 = ixy1 / 7.0;
+  vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
 
-  // Noise contribution from upper right corner
-  vec2 grad11 = texture(permTexture, Pi + vec2(ONE, ONE)).rg * 4.0 - 1.0;
-  float n11 = dot(grad11, Pf - vec2(1.0, 1.0));
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
 
-  // Blend contributions along x
-  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade(Pf.x));
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
 
-  // Blend contributions along y
-  float n_xy = mix(n_x.x, n_x.y, fade(Pf.y));
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
 
-  return n_xy;
-}
-
-highp float rand(vec2 co)
-{
-    highp float a = 12.9898;
-    highp float b = 78.233;
-    highp float c = 43758.5453;
-    highp float dt= dot(co.xy ,vec2(a,b));
-    highp float sn= mod(dt,3.14);
-    return fract(sin(sn) * c);
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+  return 2.2 * n_xyz;
 }
 
 void main(void)
 {
 	vec3 cameraPosition = -ViewMatrix[3].xyz * mat3(ViewMatrix);
-	float color = noise(2*exTexcoord);
-	vec4 c = vec4(color*0.3,color*0.3,color*0.3,1);
+	//float color = noise(2*exTexcoord);
+	//vec4 c = vec4(color*0.3,color*0.3,color*0.3,1);
+	vec3 normal = normalize(transpose(inverse(mat3(ModelMatrix))) * exNormal);
+    vec3 surfacePos = vec3(ModelMatrix * vec4(exPosition, 1));
+    
+	float m = cnoise(surfacePos*50);
 	
-	//// lighning
+	
+	float n = (1 + sin(m*5))/2;
 
 	
-    vec3 normal = normalize(transpose(inverse(mat3(ModelMatrix))) * exNormal);
-    vec3 surfacePos = vec3(ModelMatrix * vec4(exPosition, 1));
-    vec4 surfaceColor = c;//texture(Texture, exTexcoord);//
+	vec4 c = vec4(vec3(0.35,0.3,0.25) * n + vec3(0.35,0.3,0.25)*0.15*(1-n),1); //Rock colors
+	
+	//vec4 c = vec4(vec3(0.1,0.9,0.9) * n + vec3(0.1,0.1,0.1)*(1-n),1)/4; //Blue marble?
+	
+	
+	vec4 surfaceColor = c;	
+	
+	
+	//vec4 surfaceColor = c;//texture(Texture, exTexcoord);
+	
+	//// lighning   	
+	
     vec3 surfaceToLight = normalize(lightPosition - surfacePos);
     vec3 surfaceToCamera = normalize(cameraPosition - surfacePos);
-    
-    //ambient
+	
+	//ambient
     vec3 ambient = ambientCoefficient * surfaceColor.rgb * intensities;
 
     //diffuse
